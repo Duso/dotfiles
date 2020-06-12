2aad72a1aad78df90a588a330adb46c2516a854c
Kaloyan Popstoyanov <kpopstoyanov@smartsheet.com>
2020-06-13 00:07:24 +0100

    notifications

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-# TODO: stop music player
-i3lock -fe -c 2E3440
-#i3lock -fe -i $HOME/pictures/wallpapers/valley-midnight-black.png
+notify-send "DUNST_COMMAND_PAUSE"
 
+i3lock \
+	--nofork \
+	--ignore-empty-password \
+	--show-failed-attempts \
+	--color=2E3440; \
+	notify-send "DUNST_COMMAND_RESUME"
+
+# TODO: stop music player
