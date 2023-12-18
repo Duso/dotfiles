984db0df247cafb95f22ffbfe7599884e955dce5
Duso <ciganqkula@gmail.com>
2023-12-18 12:10:58 +0200

    new monitor stuff

--- scripts/init-monitors.sh
+++ scripts/init-monitors.sh
@@ -3,12 +3,17 @@
 SETUP=$(autorandr --detected)
 M1=DisplayPort-0
 M2=DisplayPort-1
+M3=HDMI-A-0
 
 if [ "$SETUP" = "desktop" ]; then
 	bspc monitor $M1 -d 1 2 3 4 5 
 	bspc monitor $M2 -d 6 7 8 9 10
 elif [ "$SETUP" = "wfh" ]; then
 	bspc monitor $M2 -d 1 2 3 4 5 6 7 8 9 10
+elif [ "$SETUP" = "proj" ]; then
+	bspc monitor $M1 -d 1 2 3 4 5
+	bspc monitor $M2 -d 6 7 8 9 10
+	bspc monitor $M3 -d 11 12
 else
 	echo 'unknown monitor setup'
 fi
